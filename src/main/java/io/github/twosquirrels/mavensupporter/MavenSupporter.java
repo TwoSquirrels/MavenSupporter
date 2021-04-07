95cac9a4899f906f2cf05c663ce435b2140c0f22
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-04-07 21:35:00 +0900

    Formatted java file

---
+++ src/main/java/io/github/twosquirrels/mavensupporter/MavenSupporter.java
@@ -0,0 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Project Name  : Maven Supporter                                           *
+ * File Name     : MavenSupporter.java                                       *
+ * Encoding      : UTF-8                                                     *
+ * Author        : TwoSquirrels                                              *
+ * Creation Date : April 7, 2021                                             *
+ *                                                                           *
+ * Copyright 2021 TwoSquirrels                                               *
+ *                                                                           *
+ * License       : Apache-2.0                                                *
+ *                 https://www.apache.org/licenses/LICENSE-2.0               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package io.github.twosquirrels.mavensupporter;
+
+/**
+ * This is the main class.
+ * @author TwoSquirrels
+ * @version 0.0.0-SNAPSHOT
+ */
+public class MavenSupporer {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
